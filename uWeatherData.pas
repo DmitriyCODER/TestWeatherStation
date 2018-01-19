d719336f9e1583b38d235fabf29601ee152c4ee3
Dmitriy Veshniakov <dv@paypro.ru>
2018-01-19 11:59:44 +0700

    * реализация интерфейса Subject в WeatherData

--- uWeatherData.pas
+++ uWeatherData.pas
@@ -1,31 +1,74 @@
 unit uWeatherData;
 
 interface
+uses
+  uIntf, Contnrs, Classes;
+
 
 type
 
-  TWeatherData=class
+  TWeatherData=class(TInterfacedObject, ISubject)
+  private
+    FObservers: TInterfaceList;
+    FTemperature:Double;
+    FHumidity:Double;
+    FPressure:Double;
   public
+    constructor Create;
     procedure MeasurementsChanged;
+    procedure RegisterObserver(O:IObserver);
+    procedure RemoveObserver(O:IObserver);
+    procedure NotifyObservers;
+    procedure SetMeasurements(const ATemp, AHumidity, APressure:Double);
   end;
 
 implementation
 
 { TWeatherData }
 
-procedure TWeatherData.MeasurementsChanged;
-var
-  temp:Double;
-  humidity:Double;
-  pressure:Double;
+constructor TWeatherData.Create;
 begin
-  temp:=GetTemperature;
-  humidity:=GetHumidity;
-  pressure:=GetPressure;
+  FObservers:=TInterfaceList.Create;
+end;
 
-  CurrentConditionsDisplay.Update(temp, humidity, pressure);
-  StatisticsDisplay.Update(temp, humidity, pressure);
-  ForecastDisplay.Update(temp, humidity, pressure);
+procedure TWeatherData.MeasurementsChanged;
+begin
+  NotifyObservers;
+end;
+
+procedure TWeatherData.NotifyObservers;
+var
+  i:Integer;
+  Observer:IObserver;
+begin
+  for i:=0 to FObservers.Count-1 do
+  begin
+    Observer:=IObserver(FObservers.Items[i]);
+    Observer.Update(FTemperature,FHumidity,FPressure);
+  end;
+end;
+
+procedure TWeatherData.RegisterObserver(O: IObserver);
+begin
+  FObservers.Add(O);
+end;
+
+procedure TWeatherData.RemoveObserver(O: IObserver);
+var
+  i:Integer;
+begin
+  i:=FObservers.IndexOf(O);
+  if i>=0 then
+    FObservers.Delete(i);
+end;
+
+procedure TWeatherData.SetMeasurements(const ATemp, AHumidity,
+  APressure: Double);
+begin
+  FTemperature:=ATemp;
+  FHumidity:=AHumidity;
+  FPressure:=APressure;
+  MeasurementsChanged;
 end;
 
 end.
